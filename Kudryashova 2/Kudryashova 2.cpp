6270bab602eb57c89bcc0582faeb28efd9b34aab
Katris04 <yekaterina.kudryashova.2004@bk.ru>
2025-10-08 19:14:40 +0500

    Add files via upload

---
+++ Kudryashova 2/Kudryashova 2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+#include <locale>
+using namespace std;
+int main() {
+	locale::global(locale(""));
+	cout << "Введите два числа";
+	int a, b;
+	cin >> a >> b;
+	cout << "сумма чисел равна" << a+b << endl;
+	if (a >= b) {
+		cout << "разность чисел равна" << a - b << endl;
+	}
+	if (a < b) {
+		cout << "разность чисел равна" << b - a << endl;
+	}
+	cout << "произведение чисел равно" << a*b << endl;
+	return 0;
+}
