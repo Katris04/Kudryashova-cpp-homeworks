1cccfd9f1e06b8334a64d66a0e70358280c3c295
Katris04 <yekaterina.kudryashova.2004@bk.ru>
2025-10-08 17:38:07 +0500

    Add files via upload

---
+++ kudryashova 1/kudryashova 1.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include <string>
+int main() {
+	std::string name;
+	std::cout << "what is your name?";
+	std::getline(std::cin, name);
+	std::cout << "Hello," << name << "!<3" << std::endl;
+	return 0;
+ }
