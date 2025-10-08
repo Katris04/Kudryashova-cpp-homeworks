288f80e27bea1b5c2f56f94eaf5af626dbabda32
Katris04 <yekaterina.kudryashova.2004@bk.ru>
2025-10-08 18:41:10 +0500

    Add files via upload

---
+++ kudryashova 4/kudryashova 2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+#include <locale>
+using namespace std;
+int main() {
+	locale::global(locale(""));
+	double number;
+	cout << "Введите число";
+	cin >> number;
+	cout << "Как double:" << number << endl;
+	int intValue = static_cast<int>(number);
+	cout << "Как int:" << intValue << endl;
+	bool boolValue = static_cast<bool>(number);
+	cout << "Число не равно 0?" << boolalpha << boolValue << endl;
+	return 0;
+}
