e4133956442eee8e63d609db073ff8af2aa2fa80
Katris04 <yekaterina.kudryashova.2004@bk.ru>
2025-10-15 19:34:39 +0500

    Add files via upload

---
+++ kudryashova3/kudryashova3.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<locale>
+using namespace std;
+int main() {
+	locale::global(locale(""));
+	int a, b;
+	cout << "введите первое число: ";
+	cin >> a;
+	cout << "введите второе число:";
+	cin >> b;
+	if (a >= b) {
+		cout << "остаток от деления равен " << a % b;
+	}
+	else {
+		cout << "остаток от деления равен " << b % a;
+	}
+	return 0;
+}
